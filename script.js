02e1a2645b3522145ceeec783129b4b4119bbc10
Norton Mourer <137120429+Nortenn@users.noreply.github.com>
2023-06-30 15:59:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const rinauthk = {
-    script: true,
-    a: true,
-    b: true,
-    c: true,
-    d: true
+  script: true,
+  a: true,
+  b: true,
+  c: true,
+  d: true,
+  'D2': "",
+  'k': {
+    '': true,
+    '': true
+  },
+  'init': () => { console['log']('Client run!')}
 }
+rinauthk['init']()
