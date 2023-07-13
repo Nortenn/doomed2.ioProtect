691512dc07182b33313eb9cabdc50b314d3e7d6a
Norton Mourer <137120429+Nortenn@users.noreply.github.com>
2023-07-13 17:35:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,7 @@
-const rinauthk = {
-  script: false,
-  a: true,
-  b: true,
-  c: true,
-  d: true,
-  code_token: 123321,
-  'D2': "",
-  'k': {
-    'Trap2808': true,
-    'TrA': true
-  },
-  owner_script: {
-    rina: false,
-    freeScript: true
+const users = {
+  isActive: true,
+  1: {
+    name: 'norton',
+    ip: 'none'
   }
 }
-
-const rand = () => {
-  return Math.random().toString(36).substr(2);
-};
-
-const token = () => {
-  return rand() + rand() + rand() + "-" + rand() + rand() + rand();
-};
-
-let tokenSave = token();
-let apiKey = token();
-
-console.log(tokenSave);
-
-const tokenHistory = [];
-
-setInterval(() => {
-  tokenSave = token();
-  apiKey = token();
-
-  const tokenObject = {
-    tokenSave,
-    apiKey
-  };
-
-  tokenHistory.push(tokenObject);
-}, 0);
-
-let connects = 0
-
-const usersDB = []
-
-const usersObject = {
-    connects
-}
-
-usersDB.push(usersObject)
